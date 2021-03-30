fb1ad83e825ca7bfdafd5655e29a496a9eeb3419
pulkit <40516537+Pulkitjndl@users.noreply.github.com>
2021-03-30 19:00:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
+const routing  = (
+   <HashRouter>
+   
+      <App />
+ 
+  </HashRouter>
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+ routing,
   document.getElementById("root")
 );
